c048292e24ecdd6f41ca632cd44a07d0a4acd4b1
JiYoungDo <carrie990129@gmail.com>
2021-03-16 18:48:59 +0900

    modify mypageFragmentRetrofitInterface

--- app/src/main/java/com/example/lifoo_v11/src/MypageFragment/interfaces/MypageFragmentRetrofitInterface.java
+++ app/src/main/java/com/example/lifoo_v11/src/MypageFragment/interfaces/MypageFragmentRetrofitInterface.java
@@ -4,10 +4,12 @@ import com.example.lifoo_v11.src.MypageFragment.models.MypageFragmentResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface MypageFragmentRetrofitInterface {
 
     // 회원 정보 조회
-    @GET("/users/2")
-    Call<MypageFragmentResponse> GetProfileTest();
+    @GET("/users/{userIdx}")
+    Call<MypageFragmentResponse> GetProfileTest(
+           @Path("userId") int param);
 }
